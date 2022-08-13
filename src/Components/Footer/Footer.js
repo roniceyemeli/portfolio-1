a97de234ed72f92bcdc37241227209f7a6793b96
unknown <vinyssusrodolphe@gmail.com>
2022-08-13 19:03:58 +0200

    footer 1

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,29 +5,13 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="footer-col">
-            <h4>Element</h4>
+      <div className="container"> 
             <ul>
               <li>Acceuil</li>
               <li>Projet</li>
               <li>Service</li>
               <li>Contact</li>
             </ul>
-          </div>
-        </div>
-        <div className="row">
-          <div className="footer-col">
-            <h4>Element</h4>
-            <ul>
-              <li>Acceuil</li>
-              <li>Projet</li>
-              <li>Service</li>
-              <li>Contact</li>
-            </ul>
-          </div>
-        </div>
       </div>
     </footer>
   );
