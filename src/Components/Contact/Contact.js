8e6ffca2091ba0418978b5e8e3cd44dd03dacb43
unknown <vinyssusrodolphe@gmail.com>
2022-08-14 06:42:13 +0200

    footer final contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <div className='contact'><h2>Contact</h2></div>
-  )
+    <>
+    <div className="label"><h2>Contact</h2></div>
+    <div className="contact">
+      <form className="form">
+        <input type="text" id="fname" name="fname" />
+        <textarea type="text" id="lname" name="lname" />
+        <button type="submit" value="Submit">Submit</button>
+      </form>
+    </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
